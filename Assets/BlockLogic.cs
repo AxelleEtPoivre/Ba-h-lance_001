d8d751782f0a05dffdd0b8845be898ce5ad932e4
Axelle <a.gain-le-marchand@etudiant.ecolemelies.fr>
2020-11-19 15:00:06 +0100

    Add rig to GameObject z
    add rig to rotate GameObject in space

--- Assets/BlockLogic.cs
+++ Assets/BlockLogic.cs
@@ -6,6 +6,8 @@ public class BlockLogic : MonoBehaviour
 {
     bool movable = true;
     float timer = 0f;
+    public GameObject rig;
+
 
     // Start is called before the first frame update
     void Start()
@@ -16,31 +18,41 @@ public class BlockLogic : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //Update the timer
-        timer += 1 * Time.deltaTime;
+        if (movable)
+        { 
+             //Update the timer
+             timer += 1 * Time.deltaTime;
 
-        //Drop
-        if(Input.GetKey(KeyCode.DownArrow) && timer > GameLogic.quickDropTime)
-        {
-            gameObject.transform.position -= new Vector3(0, 1, 0);
-            timer = 0;
-        }
+            //Drop
+            if(Input.GetKey(KeyCode.DownArrow) && timer > GameLogic.quickDropTime)
+            {
+                 gameObject.transform.position -= new Vector3(0, 1, 0);
+                 timer = 0;
+             }
 
-        else if(timer > GameLogic.dropTime)
-        {
-            gameObject.transform.position -= new Vector3(0, 1, 0);
-            timer = 0;
-        }
+            else if(timer > GameLogic.dropTime)
+            {
+                gameObject.transform.position -= new Vector3(0, 1, 0);
+                timer = 0;
+            }
            
-        //Sideways
-        if(Input.GetKeyDown(KeyCode.LeftArrow))
-        {
-            gameObject.transform.position -= new Vector3(1, 0, 0);
-        }
-        else if (Input.GetKeyDown(KeyCode.RightArrow))
-        {
-            gameObject.transform.position += new Vector3(1, 0, 0);
-        }
+            //Sideways
+            if(Input.GetKeyDown(KeyCode.LeftArrow))
+            {
+                gameObject.transform.position -= new Vector3(1, 0, 0);
+            }
 
+            else if (Input.GetKeyDown(KeyCode.RightArrow))
+            {
+                gameObject.transform.position += new Vector3(1, 0, 0);
+            }
+
+            //Rotation
+            if(Input.GetKeyDown(KeyCode.Space))
+            {
+                rig.transform.eulerAngles -= new Vector3(0, 0, 90);
+
+            }
+        }
     }
 }
